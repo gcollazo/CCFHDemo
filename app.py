839311365a8a30cf07d1b633f311b0ac24d9ab00
Giovanni Collazo <gcollazo@gmail.com>
2012-10-14 14:02:36 -0400

    Adds some redis fun

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
+import time
+
 from flask import Flask
+from flask import render_template
+
+from store import redis
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route("/")
 def hello():
-    return 'Hello: {}'.format(os.uname()[1])
+    redis.rpush('thelist',
+        'Hello: {} - {}'.format(os.uname()[1], time.time()))
+    the_list = redis.lrange('thelist', 0, -1)
+    return render_template('index.html', the_list=the_list)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
