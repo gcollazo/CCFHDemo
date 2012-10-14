820ee4a6ac150023f709bcc0393e96480ca35f5b
Giovanni Collazo <gcollazo@gmail.com>
2012-10-14 14:13:00 -0400

    More consistency using env vars

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def hello():
     return render_template('index.html', the_list=the_list)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = os.getenv('PORT', 5000)
     app.run(host='0.0.0.0', port=port)
